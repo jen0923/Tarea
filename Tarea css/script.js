e793fa1e720e6d367db6ed902373f714c6681294
jen0923 <jenrc_09@hotmail.com>
2021-06-06 11:57:28 -0600

    comit2

--- Tarea css/script.js
+++ Tarea css/script.js
@@ -10,38 +10,72 @@ if  (demo.style.display ==="none")   {
 }
 function validarVacios(resultado,operacion) {
         if(!isNaN(resultado)) {
-            imprimirMensaje(operacion,resultado);
+            imprimirMensaje(operacion, resultado);
         } else {
             document.getElementById('result').textContent = "Debe ingresar valores numericos";
         }
     }
 
-    function sumar() {
-        var Numero1= Number(document.getElementById ("myNumber1").value);   
-        var Numero2= Number(document.getElementById ("myNumber2").value);   
-        var respuesta = Numero1+Numero2;   
-        document.getElementById("result").value=respuesta;
-
-    }
-    function restar() {
-        var numero1= Number(document.getElementById ("myNumber1").value);   
-        var numero2= Number(document.getElementById ("myNumber2").value);   
-        var respuesta = numero1 - numero2;   
-        document.getElementById("result").value=respuesta;
-        
-    }
-    function multiplicar() {
-        var Numero1= Number(document.getElementById ("myNumber1").value);   
-        var Numero2= Number(document.getElementById ("myNumber2").value);   
-        var respuesta = Numero1*Numero2;   
-        document.getElementById("result").value=respuesta;
-        
-    }
-    function imprimirMensaje(operacion, resultado) {
-        document.getElementById("result").textContent = "El resultado de la" + operacion +"es" + resultado;
+    function operaciones(op)
+    {
     
-
-        var result= Number(document.getElementById ("result").value);   
-     
-
+        var ops = {
+            sumar: function sumarNumeros(n1, n2) {
+                return (parseInt(n1) + parseInt(n2));
+            },
+    
+            restar: function restarNumeros(n1, n2) {
+                return (parseInt(n1) - parseInt(n2));
+            },
+            
+            multiplicar: function multiplicarNumeros(n1, n2) {
+                return (parseInt(n1) * parseInt(n2));
+            },
+    
+    
+    
+        };
+    
+    
+    
+        var num1 = document.getElementById("num1").value;
+        var num2 = document.getElementById("num2").value;
+    
+        
+        
+        if (isNaN(parseFloat(document.getElementById('num1').value))) {
+            document.getElementById('resultado').innerHTML="<span style='color: red;'>Debe ingresar valores numericos</span>";
+            document.getElementById("num1").innerText = "0";
+            document.getElementById("num1").focus();
+            } else if (isNaN(parseFloat(document.getElementById('num2').value))) {
+            document.getElementById('resultado').innerHTML="<span style='color: red;'>Debe ingresar valores numericos</span>";
+            document.getElementById("num2").innerText = "0";
+            document.getElementById("num2").focus();
+        }
+        else {
+    
+            switch(op) {
+                case 'sumar':
+                    var resultado = ops.sumar(num1, num2);
+                    document.getElementById('resultado').innerHTML="<span style='color: green;'>"+resultado+"</span>";
+                    break;
+                case 'restar':
+                    var resultado = ops.restar(num1, num2);
+                    document.getElementById('resultado').innerHTML="<span style='color: green;'>"+resultado+"</span>";
+                    break;
+                case 'multiplicar':
+                    var resultado = ops.multiplicar(num1, num2);
+                    document.getElementById('resultado').innerHTML="<span style='color: green;'>"+resultado+"</span>";
+                    break;
+                
+    
+            }
+        }
+    
+    }
+    function imprimirMensaje(op,resultado) {
+        var resultado = ops.sumar(num1, num2);
+        var resultado = ops.restar(num1, num2);
+        var resultado = ops.multiplicar(num1, num2);
+        document.getElementById("resultado").textContent = "El resultado de la" + op +"es" + resultado; 
     }
